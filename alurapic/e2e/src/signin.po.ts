bea43d9df5da55490c445088fbae55e93dd36b6f
Daniele Leite <danii.exe@gmail.com>
2020-05-03 18:05:43 -0300

    3.4. Fazer buscas de elemento dom (exercicio)

--- alurapic/e2e/src/signin.po.ts
+++ alurapic/e2e/src/signin.po.ts
@@ -15,6 +15,8 @@ export class SignInPage {
         // BY busca pelo o que queremos buscar o elemento
         // SendKeys envia algo para o campo
         return element(by.css('input[formControlName="userName"]')).sendKeys(`flavio`);
+        // Com esse método, é possível fazer a busca do pela elemento de maneira genérica. 
+        // Nesse exemplo, é possível ver a busca pelo formControlName do input, no caso, no tipo userName.
     }
 
     pegarInputPassword() {
