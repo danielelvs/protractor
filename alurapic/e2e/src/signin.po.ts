e4e2b8ee72be771c653e7df473baebb54cddac3a
Daniele Leite <danii.exe@gmail.com>
2020-05-03 17:29:12 -0300

    2.5. Browser (Exercicio)

--- alurapic/e2e/src/signin.po.ts
+++ alurapic/e2e/src/signin.po.ts
@@ -3,7 +3,7 @@ import { browser } from 'protractor';
 export class SignInPage {
 
     acessarHome() {
-        return browser.get('');
+        return browser.get(''); // Com o get() é possível informar a rota que desejar para realizar os testes.
     }
 
 }
